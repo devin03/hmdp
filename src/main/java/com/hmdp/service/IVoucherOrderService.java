5f36b508da493bf1bdb2fc1ccb4d0e0b3b19bce0
wangdongming <wdongming_03@163.com>
2023-04-15 18:57:31 +0800

    基于synchronized实现锁的互斥

--- src/main/java/com/hmdp/service/IVoucherOrderService.java
+++ src/main/java/com/hmdp/service/IVoucherOrderService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.VoucherOrder;
 
 /**
@@ -13,4 +14,22 @@ import com.hmdp.entity.VoucherOrder;
  */
 public interface IVoucherOrderService extends IService<VoucherOrder> {
 
+    /**
+     * 秒杀优惠券抢购
+     * @param voucherId 优惠券id
+     * @return Result
+     * @author wangdongming
+     * @date 2023/04/15
+     */
+    Result seckillVoucher(Long voucherId);
+
+    /**
+     * 订单处理
+     * @param voucherId 优惠券id
+     * @param userId 用户id
+     * @return Result
+     * @author wangdongming
+     * @date 2023/04/15
+     */
+    Result orderHandle(Long voucherId, Long userId);
 }
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,20 @@
 package com.hmdp.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.utils.RedisIdGenerator;
+import com.hmdp.utils.UserHolder;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.aop.framework.AopContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -16,5 +26,61 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
+
+    @Resource
+    private SeckillVoucherServiceImpl seckillVoucherService;
+    @Resource
+    private RedisIdGenerator redisIdGenerator;
+
+    @Override
+    public Result seckillVoucher(Long voucherId) {
+        // 1.根据优惠券id查询秒杀优惠券
+        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
+        LocalDateTime now = LocalDateTime.now();
+        // 2.判断秒杀活动是否开始
+        if (voucher.getBeginTime().isAfter(now)) {
+            return Result.fail("秒杀活动还未开始");
+        }
+        // 3.判断秒杀活动是否结束
+        if (voucher.getEndTime().isBefore(now)) {
+            return Result.fail("秒杀活动已经结束");
+        }
+        // 4.判断秒杀优惠券库存是否充足
+        if (voucher.getStock() < 1) {
+            return Result.fail("库存不足");
+        }
+        // 5.一人一单
+        Long userId = UserHolder.getUser().getId();
+        //intern() 保证是同一个值
+        synchronized (userId.toString().intern()) {
+            // 获取代理对象 代理增强
+            IVoucherOrderService voucherOrderService = (IVoucherOrderService) AopContext.currentProxy();
+            return voucherOrderService.orderHandle(voucherId, userId);
+        }
+    }
+
+    @Transactional
+    public Result orderHandle(Long voucherId, Long userId) {
+        Long count = query().eq("user_id", userId).eq("voucher_id", voucherId).count();
+        if (count > 0) {
+            return Result.fail("用户已经购买过一单");
+        }
+        // 6.扣减库存
+        // 此处用了乐观锁，以库存作为条件，库存大于0就可以更新
+        boolean update = seckillVoucherService.update().setSql("stock = stock - 1")
+                .eq("voucher_id", voucherId).gt("stock", 0).update();
+        if (!update) {
+            return Result.fail("库存扣减失败");
+        }
+        // 7.生成订单
+        long orderId = redisIdGenerator.generateId("order");
+        VoucherOrder voucherOrder = new VoucherOrder();
+        voucherOrder.setId(orderId);
+        voucherOrder.setVoucherId(voucherId);
+        voucherOrder.setUserId(userId);
+        save(voucherOrder);
+        // 8.返回订单id
+        return Result.ok(orderId);
+    }
 
 }
